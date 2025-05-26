19b7f31fa2a85a04cd74e3914ef1ac3b348de475
santosbtrz <santos.btrz1@gmail.com>
2025-05-26 17:04:22 -0300

    login/reset password pages
    feat: added password reset page
    ui: implementation and styling of login and password reset screens

--- tripwise/src/app/app.routes.ts
+++ tripwise/src/app/app.routes.ts
@@ -12,14 +12,21 @@ export const routes: Routes = [
   },
   {
     path: 'login',
-    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+    loadComponent: () =>
+      import('./pages/login/login.page').then((m) => m.LoginPage),
   },
   {
     path: 'signup',
-    loadComponent: () => import('./pages/signup/signup.page').then( m => m.SignupPage)
+    loadComponent: () =>
+      import('./pages/signup/signup.page').then((m) => m.SignupPage),
   },
   {
     path: 'landing',
-    loadComponent: () => import('./pages/landing/landing.page').then( m => m.LandingPage)
+    loadComponent: () =>
+      import('./pages/landing/landing.page').then((m) => m.LandingPage),
+  },  {
+    path: 'reset-password',
+    loadComponent: () => import('./pages/reset-password/reset-password.page').then( m => m.ResetPasswordPage)
   },
+
 ];
